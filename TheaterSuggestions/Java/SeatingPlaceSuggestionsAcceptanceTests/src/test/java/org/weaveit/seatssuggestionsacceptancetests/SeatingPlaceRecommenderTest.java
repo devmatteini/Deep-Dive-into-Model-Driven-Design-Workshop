2d8603f10e61e48b08018ba97f25c41a0ff3a3bb
Kenny Baas-Schwegler <kenny@baasie.com>
2024-11-04 12:47:19 +0100

    fixed last test

--- TheaterSuggestions/Java/SeatingPlaceSuggestionsAcceptanceTests/src/test/java/org/weaveit/seatssuggestionsacceptancetests/SeatingPlaceRecommenderTest.java
+++ TheaterSuggestions/Java/SeatingPlaceSuggestionsAcceptanceTests/src/test/java/org/weaveit/seatssuggestionsacceptancetests/SeatingPlaceRecommenderTest.java
@@ -16,23 +16,6 @@ class SeatingPlaceRecommenderTest {
      */
 
     @Test
-    void suggest_two_seats_when_Auditorium_contains_all_available_seats() throws IOException {
-        // Lincoln-17
-        //
-        //     1   2   3   4   5   6   7   8   9  10
-        //  A: 2   2   1   1   1   1   1   1   2   2
-        //  B: 2   2   1   1   1   1   1   1   2   2
-        final String showId = "17";
-        final int partyRequested = 2;
-
-        var auditoriumSeatings =
-                new AuditoriumSeatings(new AuditoriumLayoutRepository(), new ReservationsProvider());
-        var seatingPlaceRecommender = new SeatingPlaceRecommender(auditoriumSeatings);
-        var suggestionsMade = seatingPlaceRecommender.makeSuggestion(showId, partyRequested);
-
-        assertThat(suggestionsMade.seatNames(PricingCategory.SECOND)).containsExactly("A1, A2");
-    }
-    @Test
     void suggest_one_seat_when_Auditorium_contains_one_available_seat_only() throws IOException {
         // Ford Auditorium-1
         //       1   2   3   4   5   6   7   8   9  10
@@ -66,6 +49,24 @@ class SeatingPlaceRecommenderTest {
         assertEquals(partyRequested, suggestionsMade.partyRequested(), "Party requested should match");
         assertEquals(showId, suggestionsMade.showId(), "Show ID should match");
         assertInstanceOf(SuggestionNotAvailable.class, suggestionsMade, "Suggestions made should be an instance of SuggestionNotAvailable");
+    }
+
+    @Test
+    void suggest_two_seats_when_Auditorium_contains_all_available_seats() throws IOException {
+        // Lincoln-17
+        //
+        //     1   2   3   4   5   6   7   8   9  10
+        //  A: 2   2   1   1   1   1   1   1   2   2
+        //  B: 2   2   1   1   1   1   1   1   2   2
+        final String showId = "17";
+        final int partyRequested = 2;
+
+        var auditoriumSeatings =
+                new AuditoriumSeatings(new AuditoriumLayoutRepository(), new ReservationsProvider());
+        var seatingPlaceRecommender = new SeatingPlaceRecommender(auditoriumSeatings);
+        var suggestionsMade = seatingPlaceRecommender.makeSuggestion(showId, partyRequested);
+
+        assertThat(suggestionsMade.seatNames(PricingCategory.SECOND)).containsExactly("A1", "A2", "A9", "A10", "B1", "B2");
     }
 
    @Test
