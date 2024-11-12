debf1a48a37fb98d9609e7537c9c5ca47f9e61fd
Cosimo Matteini <dev.matteini@gmail.com>
2024-11-12 11:18:17 +0100

    Offer_adjacent_seats_nearer_the_middle_of_the_row_when_the_middle_is_reserved

--- TheaterSuggestions/CSharp/ScreeningSeatingArrangementSuggestions.Tests/UnitTests/RowShould.cs
+++ TheaterSuggestions/CSharp/ScreeningSeatingArrangementSuggestions.Tests/UnitTests/RowShould.cs
@@ -134,6 +134,32 @@ public class RowShould
         Check.That(seatingPlaces).ContainsExactly(a3, a4, a5);
     }
 
+
+    [Test]
+    public void Offer_adjacent_seats_nearer_the_middle_of_the_row_when_the_middle_is_reserved()
+    {
+        const int partySize = 3;
+
+        var a1 = new SeatingPlace("A", 1, PricingCategory.Second, SeatingPlaceAvailability.Available);
+        var a2 = new SeatingPlace("A", 2, PricingCategory.Second, SeatingPlaceAvailability.Available);
+        var a3 = new SeatingPlace("A", 3, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a4 = new SeatingPlace("A", 4, PricingCategory.First, SeatingPlaceAvailability.Reserved);
+        var a5 = new SeatingPlace("A", 5, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a6 = new SeatingPlace("A", 6, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a7 = new SeatingPlace("A", 7, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a8 = new SeatingPlace("A", 8, PricingCategory.First, SeatingPlaceAvailability.Available);
+        var a9 = new SeatingPlace("A", 9, PricingCategory.Second, SeatingPlaceAvailability.Reserved);
+        var a10 = new SeatingPlace("A", 10, PricingCategory.Second, SeatingPlaceAvailability.Available);
+
+        var row = new Row("A", [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10]);
+
+        var seatsWithDistance = OfferSeatsNearerTheMiddleOfTheRow(row);
+
+        var seatingPlaces = OfferAdjacentSeats(seatsWithDistance, partySize);
+
+        Check.That(seatingPlaces).ContainsExactly(a5, a6, a7);
+    }
+
     // Deep Modeling: probing the code should start with a prototype.
     private static IEnumerable<SeatingPlace> OfferAdjacentSeats(IEnumerable<SeatingPlaceWithDistance> seatsWithDistance,
         int partySize)
@@ -143,7 +169,8 @@ public class RowShould
         var neverBeAdjacentSeat = new SeatingPlaceWithDistance(
             new SeatingPlace("Z", -1, PricingCategory.Ignore, SeatingPlaceAvailability.Available), -1);
         var previousSeatingPlaceWithDistance = neverBeAdjacentSeat;
-        foreach (var seatingPlaceWithDistance in seatsWithDistance.OrderBy(s => s.SeatingPlace.Number))
+        foreach (var seatingPlaceWithDistance in seatsWithDistance.Where(s => s.SeatingPlace.IsAvailable())
+                     .OrderBy(s => s.SeatingPlace.Number))
         {
             if (previousSeatingPlaceWithDistance.SeatingPlace.Number + 1 ==
                 seatingPlaceWithDistance.SeatingPlace.Number)
